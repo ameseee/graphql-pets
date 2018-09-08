12f1128974c5a14324970d0565f568296ee70aa7
Amy Holt <amy.elizabeth.holt@gmail.com>
2018-09-08 14:54:08 -0600

    Initial commit - set up boilerplate for lesson

---
+++ schema/schema.js
@@ -0,0 +1,20 @@
+const graphql = require('graphql');
+const axios = require('axios');
+
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema,
+  GraphQLList,
+  GraphQLNonNull
+} = graphql;
+
+
+// types
+// root query
+// mutations
+
+module.exports = new GraphQLSchema({
+  //export root query and mutations
+});
---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+// bring in GraphQL
+
+const app = express();
+
+// user GraphQL
+
+app.listen(4000, () => {
+  console.log('Listening on 4000...');
+});
