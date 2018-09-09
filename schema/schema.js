bb7d0c7051eacc626407bcda4ddca31f7b403bce
Amy Holt <amy.elizabeth.holt@gmail.com>
2018-09-09 13:12:01 -0600

    Add pet and company types

--- schema/schema.js
+++ schema/schema.js
@@ -12,6 +12,39 @@ const {
 
 
 // types
+
+const PetType = new GraphQLObjectType({
+  name: 'Pet',
+  fields: () => ({
+    id: { type: GraphQLString },
+    firstName: { type: GraphQLString },
+    age: { type: GraphQLInt },
+    company: {
+      type: CompanyType,
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+          .then(response => response.data);
+      }
+     }
+  })
+});
+
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    pets: {
+      type: new GraphQLList(PetType),
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/companies/${parentValue.id}/pets`)
+        .then(response => response.data);
+      }
+    }
+  })
+});
+
 // root query
 
 const RootQuery = new GraphQLObjectType({
